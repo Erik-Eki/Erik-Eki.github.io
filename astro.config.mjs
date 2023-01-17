feca747b9b67ae50ec73fbf8afec2fb862a91cd8
Erik-Eki <eriko.oy38@gmail.com>
2023-01-17 22:54:42 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://erik-eki.github.io",
+    site: "https://erikhuuskonen.fi"//"https://erik-eki.github.io",
     // Docs say don't set base if url is .github.io or using custom domain
     //base: "Erik-Eki.github.io"
 });
