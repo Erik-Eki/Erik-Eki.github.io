498f41748daf91489a71fb1a832356a981100d18
Erik-Eki <eriko.oy38@gmail.com>
2023-01-17 21:27:44 +0200

    github workflow init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://erik-eki.github.io",
+    // Docs say don't set base if url is .github.io or using custom domain
+    //base: "Erik-Eki.github.io"
+});
